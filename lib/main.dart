d43b2f3e0c3ce8fdfe42b4f98174c38779ea4169
artimmy <arthurtimmy@hotmail.com>
2022-02-10 15:10:31 -0300

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:bitcoin_price/home.dart';
 import 'package:flutter/material.dart';
 
 void main()
 {
-  
+  runApp( const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
